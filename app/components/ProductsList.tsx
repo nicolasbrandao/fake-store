3a3bc9884a23bc4a06f719a0f5fd343403abd7bd
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-04-24 17:25:02 -0300

    feat: implement base categories list

--- app/components/ProductsList.tsx
+++ app/components/ProductsList.tsx
@@ -1,5 +1,36 @@
-import React from 'react'
+'use client'
 
-export default function ProductsList() {
-  return <div>ProductsList</div>
+import { useEffect, useContext } from 'react'
+import { ProductType } from '@/types'
+import classNames from 'classnames'
+import fetchProducts from '../lib/fetchProducts'
+import { actionTypes, StoreContext } from '../context/store'
+import ProductCard from './common/ProductCard'
+
+export default function Showcase() {
+  const { state, dispatch } = useContext(StoreContext)
+
+  useEffect(() => {
+    fetchProducts().then((data) => {
+      dispatch({
+        type: actionTypes.fetchAllProducts,
+        payload: data,
+      })
+    })
+  }, [dispatch])
+
+  const productsContainerClass = classNames(
+    'grid',
+    'grid-cols-1',
+    'lg:grid-cols-3',
+    'gap-4'
+  )
+
+  return (
+    <section className={productsContainerClass}>
+      {state.productsList.map((product: ProductType) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </section>
+  )
 }
