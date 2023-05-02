f5e776d3c65599ceeabc15996774c2a5cbce09fc
Nícolas Brandão <nicolas.brandaor@gmail.com>
2023-05-02 08:36:32 -0300

    fix: logos alignment

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -32,7 +32,7 @@ export default function Footer() {
     'bottom-0'
   )
 
-  const logosContainerClass = classNames('flex', 'flex-col', 'gap-4')
+  const logosContainerClass = classNames('flex', 'flex-col', 'gap-4', 'items-center')
 
   const apiContainerClass = classNames('flex', 'gap-2', 'items-center')
 
